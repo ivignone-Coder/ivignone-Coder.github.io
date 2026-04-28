34f12fed7c45aa324aa0d41b5cf2250d1ed3c6e9
Isaiah Vignone <ivignone@asu.edu>
2026-04-27 18:39:59 -0700

    Update Events.js
    Got rid of the comments I had for myself

--- Week6/Interactivity Classwork/Events.js
+++ Week6/Interactivity Classwork/Events.js
@@ -1,9 +1,3 @@
-/*let gather = document.getElementById("onclick");
-
-function banana() {
-    console.log("Pressed!!");
-}*/
-
 document.getElementById("onclick").addEventListener("click", function() {
     alert("Pressed");
 });
