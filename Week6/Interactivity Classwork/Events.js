63ab967e81c0914aa3c0da2384aec44ad08e4fe1
Isaiah Vignone <ivignone@asu.edu>
2026-04-27 18:39:09 -0700

    Add files via upload
    Uploaded Interactivity Classwork

---
+++ Week6/Interactivity Classwork/Events.js
@@ -0,0 +1,9 @@
+/*let gather = document.getElementById("onclick");
+
+function banana() {
+    console.log("Pressed!!");
+}*/
+
+document.getElementById("onclick").addEventListener("click", function() {
+    alert("Pressed");
+});
